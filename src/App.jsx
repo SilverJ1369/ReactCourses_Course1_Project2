6563e5c67c423d075f68f0d53611f082cb318e3f
SilverJ1369 <35865016+SilverJ1369@users.noreply.github.com>
2024-09-23 07:37:31 -0500

    Lesson 90: lifting computed values up

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import GameBoard from "./components/GameBoard"
 import Log from "./components/Log"
 import { WINNING_COMBINATIONS } from "./winning-combinations"
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+]
+
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X'
 
@@ -20,6 +26,25 @@ function App() {
   // const [activePlayer, setActivePlayer] = useState('X')
 
   const activePlayer = deriveActivePlayer(gameTurns)
+
+  let gameBoard = initialGameBoard;
+
+    for (const turn of gameTurns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+
+        gameBoard[row][col] = player
+    }
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard
+    const secondSquareSymbol
+    const thirdSquareSymbol
+
+    if (isWinningCombination) {
+      console.log(`Player ${activePlayer} wins!`)
+    }
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X')
@@ -40,7 +65,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
       </div>
       <Log turns={gameTurns} />
     </main>
